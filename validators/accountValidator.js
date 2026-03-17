3d4740d6b23b19412dab7af5c751b89578b843f0
Nathanabe <99434652+Nathanabe@users.noreply.github.com>
2026-03-17 18:59:39 +0100

    Add validation schemas for registration and login

--- validators/accountValidator.js
+++ validators/accountValidator.js
@@ -1,7 +1,21 @@
 const Joi = require("joi");
 
+// Update account
 exports.updateAccountSchema = Joi.object({
   name: Joi.string().min(3),
   email: Joi.string().email(),
   password: Joi.string().min(6),
+});
+
+// Registration schema
+exports.registerSchema = Joi.object({
+  name: Joi.string().min(3).required(),
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
+});
+
+// Login schema
+exports.loginSchema = Joi.object({
+  email: Joi.string().email().required(),
+  password: Joi.string().min(6).required(),
 });
